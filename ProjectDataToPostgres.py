ca056dfd9063e3dea14c48afb992002af5b760fe
Selman <selmank@gmail.com>
2024-05-04 16:31:38 +0200

    Update ProjectDataToPostgres.py path

--- ProjectDataToPostgres.py
+++ ProjectDataToPostgres.py
@@ -4,20 +4,22 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 ########################################
 # Update connection string information #
 ########################################
-host = "<<host>>"
-user = "<<user>>"
-password = "<<password>>"
+host = "az-postgres-server.postgres.database.azure.com"
+user = "adminpostgres"
+password = "Postgres@123"
 
 # Create a new DB
 sslmode = "require"
 dbname = "postgres"
-conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(
+    host, user, dbname, password, sslmode
+)
 conn = psycopg2.connect(conn_string)
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 print("Connection established")
 
 cursor = conn.cursor()
-cursor.execute('DROP DATABASE IF EXISTS udacityproject')
+cursor.execute("DROP DATABASE IF EXISTS udacityproject")
 cursor.execute("CREATE DATABASE udacityproject")
 # Clean up initial connection
 conn.commit()
@@ -26,10 +28,13 @@ conn.close()
 
 # Reconnect to the new DB
 dbname = "udacityproject"
-conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(
+    host, user, dbname, password, sslmode
+)
 conn = psycopg2.connect(conn_string)
 print("Connection established")
 cursor = conn.cursor()
+
 
 # Helper functions
 def drop_recreate(c, tablename, create):
@@ -37,10 +42,11 @@ def drop_recreate(c, tablename, create):
     c.execute(create)
     print("Finished creating table {0}".format(tablename))
 
+
 def populate_table(c, filename, tablename):
-    f = open(filename, 'r')
+    f = open(filename, "r")
     try:
-        cursor.copy_from(f, tablename, sep=",", null = "")
+        cursor.copy_from(f, tablename, sep=",", null="")
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error: %s" % error)
@@ -48,9 +54,10 @@ def populate_table(c, filename, tablename):
         cursor.close()
     print("Finished populating {0}".format(tablename))
 
+
 # Create Rider table
 table = "rider"
-filename = './data/riders.csv'
+filename = "./data/riders.csv"
 create = "CREATE TABLE rider (rider_id INTEGER PRIMARY KEY, first VARCHAR(50), last VARCHAR(50), address VARCHAR(100), birthday DATE, account_start_date DATE, account_end_date DATE, is_member BOOLEAN);"
 
 drop_recreate(cursor, table, create)
@@ -58,7 +65,7 @@ populate_table(cursor, filename, table)
 
 # Create Payment table
 table = "payment"
-filename = './data/payments.csv'
+filename = "./data/payments.csv"
 create = "CREATE TABLE payment (payment_id INTEGER PRIMARY KEY, date DATE, amount MONEY, rider_id INTEGER);"
 
 drop_recreate(cursor, table, create)
@@ -66,7 +73,7 @@ populate_table(cursor, filename, table)
 
 # Create Station table
 table = "station"
-filename = './data/stations.csv'
+filename = "./data/stations.csv"
 create = "CREATE TABLE station (station_id VARCHAR(50) PRIMARY KEY, name VARCHAR(75), latitude FLOAT, longitude FLOAT);"
 
 drop_recreate(cursor, table, create)
@@ -74,7 +81,7 @@ populate_table(cursor, filename, table)
 
 # Create Trip table
 table = "trip"
-filename = './data/trips.csv'
+filename = "./data/trips.csv"
 create = "CREATE TABLE trip (trip_id VARCHAR(50) PRIMARY KEY, rideable_type VARCHAR(75), start_at TIMESTAMP, ended_at TIMESTAMP, start_station_id VARCHAR(50), end_station_id VARCHAR(50), rider_id INTEGER);"
 
 drop_recreate(cursor, table, create)
