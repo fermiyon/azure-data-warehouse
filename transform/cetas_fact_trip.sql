609e1eb2ba18b92759a7339ce71a9764e820e5fb
Selman <selmank@gmail.com>
2024-05-05 05:48:47 +0200

    Update fact trip rider_age according to review

--- transform/cetas_fact_trip.sql
+++ transform/cetas_fact_trip.sql
@@ -13,7 +13,15 @@ SELECT st.trip_id,
   st.start_station_id,
   st.end_station_id,
   DATEDIFF(HOUR, st.start_at, st.ended_at) AS duration,
-  DATEDIFF(YEAR, sr.birthday, st.start_at) AS rider_age
+  (DATEDIFF(year, r.birthday,
+    CONVERT(Datetime, SUBSTRING([started_at], 1, 19),120)) - (
+        CASE WHEN MONTH(r.birthday) > MONTH(CONVERT(Datetime, SUBSTRING([started_at], 1, 19),120))
+        OR MONTH(r.birthday) =
+            MONTH(CONVERT(Datetime, SUBSTRING([started_at], 1, 19),120))
+        AND DAY(r.birthday) >
+            DAY(CONVERT(Datetime, SUBSTRING([started_at], 1, 19),120))
+        THEN 1 ELSE 0 END
+    )) AS rider_age
 FROM [dbo].[staging_trip] st
   JOIN [dbo].[staging_rider] sr ON sr.rider_id = st.rider_id;
 GO -- Query the newly created CETAS external table
